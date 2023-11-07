f9ff3ff12c57c32037c5e877369fce538cc4453e
rspakal <gibsrom@gmail.com>
2023-11-07 13:07:22 +0100

    Add project files.

---
+++ SF1433/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF1433
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortedContacts = from p in phoneBook orderby p.Name, p.LastName select p;
+            while (true)
+            {
+                var input = Console.ReadKey().KeyChar;
+                var parsed = int.TryParse(input.ToString(), out int pageNumber);
+                if (!(parsed || pageNumber < 1) || (pageNumber > 3))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                else
+                {
+                    var pageContent = sortedContacts.Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                    Console.WriteLine();
+                }
+            }
+
+        }
+        public class Contact
+        {
+            public Contact(string name, string lastName, long phoneNumber, string email)
+            {
+                Name = name;
+                LastName = lastName;
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+            public string Name { get; }
+            public string LastName { get; }
+            public long PhoneNumber { get; }
+            public string Email { get; }
+        }
+    }
+}
